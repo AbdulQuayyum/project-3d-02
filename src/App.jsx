5934c2a074a86b3735a989ae3eea981e08a383ac
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-07-07 14:36:10 +0100

    Added background, working on car model...

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { EffectComposer, DepthOfField, Bloom, ChromaticAberration } from "@react
 import { BlendFunction } from "postprocessing";
 import { CubeCamera, Environment, OrbitControls, PerspectiveCamera } from "@react-three/drei";
 
+import { Boxes, Car, Ground, FloatingGrid, Rings } from "./Components/Index"
+
 function CarShow() {
   return (
     <>
@@ -15,6 +17,15 @@ function CarShow() {
       <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
 
       <color args={[0, 0, 0]} attach="background" />
+
+      <CubeCamera resolution={256} frames={Infinity}>
+        {(texture) => (
+          <>
+            <Environment map={texture} />
+            {/* <Car /> */}
+          </>
+        )}
+      </CubeCamera>
 
       <spotLight
         color={[1, 0.25, 0.7]}
@@ -34,6 +45,27 @@ function CarShow() {
         castShadow
         shadow-bias={-0.0001}
       />
+      <Ground />
+      <FloatingGrid />
+      <Boxes />
+      <Rings />
+
+      <EffectComposer>
+        {/* <DepthOfField focusDistance={0.0035} focalLength={0.01} bokehScale={3} height={480} /> */}
+        <Bloom
+          blendFunction={BlendFunction.ADD}
+          intensity={1.3} // The bloom intensity.
+          width={300} // render width
+          height={300} // render height
+          kernelSize={5} // blur kernel size
+          luminanceThreshold={0.15} // luminance threshold. Raise this value to mask out darker elements in the scene.
+          luminanceSmoothing={0.025} // smoothness of the luminance threshold. Range is [0, 1]
+        />
+        <ChromaticAberration
+          blendFunction={BlendFunction.NORMAL} // blend mode
+          offset={[0.0005, 0.0012]} // color offset
+        />
+      </EffectComposer>
     </>
   )
 }
